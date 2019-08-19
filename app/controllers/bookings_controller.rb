8e6d69d54237883b3ee5a05917e80f277eebcf55
Louis-Olivier Pelletier <louisolivierpelletier@gmail.com>
2019-08-19 17:13:46 -0400

    Booking's controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,48 @@
 class BookingsController < ApplicationController
+  def index
+    @bookings = Booking.all
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  def new
+    @boat = Boat.find(params[:boat_id])
+    @booking = Booking.new
+  end
+
+  def create
+    @boat = Boat.find(params[:boat_id])
+    @booking = Booking.new(booking_params)
+    @booking.boat = @boat
+    if @booking.save
+      redirect_to booking_path(@booking.boat)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @booking = Booking.find(params[:id])
+  end
+
+  # Pas certain de la formulation pour l'update
+  def update
+    @booking = Booking.find(params[:id])
+    @booking.booking_params
+    redirect_to booking_path(@booking.boat)
+  end
+
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to bookings_path
+  end
+
+  private
+
+  def booking_params
+    params.require(:boat).permit(:name, :location, :description, :capacity, :year, :num_of_cabin, :category, :user_id, :brand_id)
+  end
 end
