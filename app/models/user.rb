98db6daa4ef9c4e8629c3ddef430f131a42f865c
Younes Kamel <mia.san.younes@gmail.com>
2019-08-19 17:54:10 -0400

    devise added to suer

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :confirmable, :trackable, :timoutable
+         :confirmable, :trackable, :timeoutable
 end
