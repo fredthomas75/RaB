346bea5a7f078b9b9acc5cd7c6278584491ec318
Frederic Thomas <fred@mt2lab.com>
2019-08-19 14:34:30 -0400

    with updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :boats do
-    resources :bookings
+    resources :bookings, only: [:new, :create]
   end
+  resources :bookings, except: [:new, :create]
 end
