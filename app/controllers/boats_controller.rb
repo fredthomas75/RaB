f9d39759107bec08b383b2a8856e8e0052c8c102
Younes Kamel <mia.san.younes@gmail.com>
2019-08-19 16:54:30 -0400

    boat controller + boat.rb

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -1,2 +1,53 @@
 class BoatsController < ApplicationController
+  before_action :set_boat, only: [:show, :edit, :update, :destroy]
+
+  # GET /boats
+  def index
+    @boats = Boat.all
+  end
+
+  # GET /boats/:id
+  def show
+  end
+
+  # GET /boats/new
+  def new
+    @boat = Boat.new
+  end
+
+  # POST /boats
+  def create
+    @boat = Boat.new(boat_params)
+
+    @boat.save
+
+    redirect_to boat_path(boat)
+  end
+
+  # GET /boats/:id/edit
+  def edit
+  end
+
+  # PATCH /boats/:id
+  def update
+    @boat.update(boat_params)
+    redirect_to boat_path(@boat)
+  end
+
+  # DELETE /boat/:id
+  def destroy
+    @boat.destroy
+
+    redirect_to boats_path
+  end
+
+  private
+
+  def boat_params
+    params.require(:boat).permit(:name, :address)
+  end
+
+  def set_boat
+    @boat = Boat.find(params[:id])
+  end
 end
--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,5 +1,5 @@
 class Boat < ApplicationRecord
   # belongs_to :user
   belongs_to :brand_model
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
 end
