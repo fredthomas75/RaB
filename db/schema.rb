108bf60e9bc819825079a6d2ddf41e103592ffcb
l b <poumtak@yahoo.fr>
2019-08-20 14:35:16 -0400

    added name in the sign up form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_20_180733) do
+ActiveRecord::Schema.define(version: 2019_08_20_181253) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
