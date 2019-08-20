7aa22ab7056e93f7c683382e1d8da39d6406ada1
Younes <mia.san.younes@gmail.com>
2019-08-20 11:15:58 -0400

    edit destroy et update sont reservées à owner

--- app/policies/boat_policy.rb
+++ app/policies/boat_policy.rb
@@ -4,4 +4,23 @@ class BoatPolicy < ApplicationPolicy
       scope.all
     end
   end
+
+   def update?
+    user_is_owner?
+  end
+
+  def edit?
+    user_is_owner?
+  end
+
+  def destroy?
+    user_is_owner?
+  end
+
+  private
+
+  def user_is_owner?
+    return record.user_id == user
+  end
+  # record c'est le bateau
 end
