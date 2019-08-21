b36b00a6e0579073d8d3f71a443292a5182288c1
Louis-Olivier Pelletier <louisolivierpelletier@gmail.com>
2019-08-21 13:32:46 -0400

    Prix, lat, log dans schema

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -4,4 +4,8 @@ class Boat < ApplicationRecord
   belongs_to :brand_model
   has_many :bookings, dependent: :destroy
   mount_uploader :photo, PhotoUploader
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_20_181253) do
+ActiveRecord::Schema.define(version: 2019_08_21_172830) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,9 @@ ActiveRecord::Schema.define(version: 2019_08_20_181253) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.string "photo"
+    t.float "latitude"
+    t.float "longitude"
+    t.integer "price"
     t.index ["brand_model_id"], name: "index_boats_on_brand_model_id"
     t.index ["user_id"], name: "index_boats_on_user_id"
   end
